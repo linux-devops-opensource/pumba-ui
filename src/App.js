d8b41df53c05feb447b711f8ee4f5cfdb00a9cf5
Shir Monether <ShirMon@localhost.localdomain>
2021-01-25 12:15:53 +0200

    added loading screen gif

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { HashRouter, Route, Switch, Router  } from 'react-router';
 import 'react-fontawesome'
 import { createBrowserHistory } from "history";
+import { TextareaAutosize } from '@material-ui/core';
+import './App.css'
+import pumbaloading from './images/pumba-loading.gif'
 
 // import '@fortawesome/fontawesome-free/css/all.min.css'
 
@@ -11,8 +14,8 @@ const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const history = createBrowserHistory({ basename: baseUrl });
 const TheLayout = React.lazy(() => import('./containers/Layout/Layout'));
 const loading = (
-  <div >
-    <div >blaaa</div>
+  <div className="Loading">
+    <img src={pumbaloading} />
   </div>
 )
 const App = (props) => {
